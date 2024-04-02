bd6b7f8eeda000a2fb6d294dbf340f101f25b017
Suhaimee kaje <111553531+Suhaimee123@users.noreply.github.com>
2024-04-03 01:59:05 +0700

    map1 ใส animation

--- FanalGamas/Assets/Scripts/playercontrol.cs
+++ FanalGamas/Assets/Scripts/playercontrol.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float moveSpeed; // Corrected variable name
-
-    public bool isMoving;
+    public float moveSpeed;
+    private bool isMoving;
     private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>(); // Corrected method name
+    }
 
     private void Update()
     {
         if (!isMoving)
         {
-            input.x = Input.GetAxisRaw("Horizontal"); // Corrected method name and variable name
-            input.y = Input.GetAxisRaw("Vertical"); // Corrected method name
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            Debug.Log("This is input.x: " + input.x); // Removed extra Debug prefix
+            Debug.Log("This is input.y: " + input.y);
+
+            if (input.x != 0) input.y = 0; // Corrected inequality operator
 
             if (input != Vector2.zero)
             {
-                var targetPos = transform.position; // Corrected variable name
+                animator.SetFloat("moveX", input.x); // Corrected method name
+                animator.SetFloat("moveY", input.y); // Corrected method name
+
+                var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
                 StartCoroutine(Move(targetPos));
             }
         }
+
+        animator.SetBool("isMoving", isMoving); // Added missing semicolon and fixed indentation
     }
 
     IEnumerator Move(Vector3 targetPos)
@@ -32,8 +47,7 @@ public class NewBehaviourScript : MonoBehaviour
         isMoving = true;
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime); // Corrected variable name and added missing multiplication sign
-        
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
         transform.position = targetPos;
